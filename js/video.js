184d066853086118bef3e6dd006809d0f903c3d8
Matthew Garvin <45524793+mtthwgrvn@users.noreply.github.com>
2020-02-28 12:16:40 -0500

    closing in on assignment completion

--- js/video.js
+++ js/video.js
@@ -1,52 +1,73 @@
 var video;
 
 function getVid(){
-	video = document.querySelector("#myVideo"); 
+	video = document.querySelector("#myVideo");
 }
 
-function playVid() { 
+function playVid() {
 
 	console.log("Play Video");
-} 
+	if (video.paused)
+		video.play();
+		changeVolume();
+}
 
-function pauseVid() { 
+function pauseVid() {
 
 	console.log("Pause Video");
-} 
+	if (!video.paused)
+		video.pause();
+}
 
-function decreaseSpeed() { 
+function decreaseSpeed() {
+	video.playbackRate = video.playbackRate * .8;
+	console.log("Speed is "+ video.playbackRate);
 
-  	console.log("Speed is "+ );
-} 
+}
 
 function increaseSpeed() {
-
-	console.log("Speed is "+ );
-} 
+	video.playbackRate = video.playbackRate * 1.25;
+	console.log("Speed is "+ video.playbackRate);
+}
 
 function skipAhead() {
+	video.currentTime = video.currentTime + 60;
+	if (video.ended) {
+		video.currentTime = 0;
+		pauseVid();
+		video.playbackRate = 1;
+	}
+	console.log("Current location is "+ video.currentTime);
+}
 
-	console.log("Current location is "+ );
-} 
-
-function mute() { 
-  	
-  		console.log("Unmuted");
-  	
-      	console.log("Muted");
+function mute() {
+  	if (video.muted) {
+		video.muted = false;
+		document.querySelector("#mute").textContent = "Mute";
+		console.log("Unmuted");
+	  }
+	else {
+		video.muted = true;
+		document.querySelector("#mute").textContent = "Unmute";
+		console.log("Muted");
+	}
 }
 
 function changeVolume() {
-;	console.log("Volume is ");
+	var volume = document.querySelector("#volumeSlider").value;
+	video.volume = volume / 100;
+	var volume_span = document.querySelector("#volume");
+	volume_span.textContent = volume + "%";
+	console.log("Volume is " + volume);
 }
-       
 
-function gray() { 
 
+function gray() {
+	video.classList.add("grayscale")
 	console.log("In grayscale")
 }
 
 function color() {
-
-	console.log("In color") 
+	video.classList.remove("grayscale")
+	console.log("In color")
 }
